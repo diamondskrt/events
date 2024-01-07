0bfcdb0d612220c78abc0ce313066ceacb99e347
diamondskrt <create7321@gmail.com>
2024-01-07 18:56:41 +0300

    feat: mongodb, clerk and webhooks

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions';
+import { clerkClient } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -52,8 +55,6 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(id);
-
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
@@ -67,7 +68,40 @@ export async function POST(req: Request) {
       photo: image_url,
     };
 
-    console.log(user);
+    const newUser = await createUser(user);
+
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
+
+    return NextResponse.json({ message: 'OK', user: newUser });
+  }
+
+  if (eventType === 'user.updated') {
+    const { id, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      userName: username,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
+
+    const updatedUser = await updateUser(id, user);
+
+    return NextResponse.json({ message: 'OK', user: updatedUser });
+  }
+
+  if (eventType === 'user.deleted') {
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser(id);
+
+    return NextResponse.json({ message: 'OK', user: deletedUser });
   }
 
   return new Response('', { status: 200 });
